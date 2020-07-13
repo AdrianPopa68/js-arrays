30927d9028dc944e8841cf447446d0733811c50c
AdrianPopa68 <52835413+AdrianPopa68@users.noreply.github.com>
2020-07-13 14:56:42 +0300

    Create lab.js

---
+++ lab.js
@@ -0,0 +1,6 @@
+/*We have an array of school grades achieved at the end of primary cycle by students in a given class*/
+var results = [9.20, 8.35, 9.98, 9.21, 9.12, 9.80, 9.44, 8.89, 9.12, 10, 10, 8.35, 7.67];
+
+/*What grade was achieved by the 3rd student*/
+var score3 = results[2];
+alert("Student no. 3 achieved a score of " + score3);
